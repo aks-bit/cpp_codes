96066d75a7e3247c8798f4e6e8256b70b090f739
aks-bit <aksembfw@gmail.com>
2023-03-18 21:25:20 +0530

    commit-2

--- string_print.cpp
+++ string_print.cpp
@@ -9,14 +9,17 @@ public:
   
   void PrintMyString()
        {
-           cout << "This is my string: " << MyString;
+           cout << "This is my string: " << MyString<< "\n";
        }
 };
 int main() {
    StringPrint string1;
+   StringPrint string2;
    
    string1.MyString = "Hi Anand, welcome in cpp programing!";
+   string2.MyString = "Thanks! This is my first cpp program";
    string1.PrintMyString();
+   string2.PrintMyString();
    
    return 0;
 }
