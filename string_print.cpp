272c9dbb7ee880a2adaafd95befca76efa871afb
aks-bit <aksembfw@gmail.com>
2023-03-18 21:39:24 +0530

    commit-3

--- string_print.cpp
+++ string_print.cpp
@@ -15,11 +15,14 @@ public:
 int main() {
    StringPrint string1;
    StringPrint string2;
+   StringPrint string3;
    
    string1.MyString = "Hi Anand, welcome in cpp programing!";
-   string2.MyString = "Thanks! This is my first cpp program";
+   string2.MyString = "Thanks! This is my first cpp program.";
+   string3.MyString = "Hope, I will become a good cpp programmer.";
    string1.PrintMyString();
    string2.PrintMyString();
+   string3.PrintMyString();
    
    return 0;
 }
